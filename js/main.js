fd3c76c6caa068271213d56fa02348cd233c32b3
Moon Bombo <bombo96@naver.com>
2022-01-16 16:50:35 +0900

    Add lodash cdn, gsap cdn

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder','');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log('scroll!');
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    }); /* gsap.to(요소, 지속시간, 옵션); */
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// lodash cdn _.throttle(function, ms)
