fe042ecb3fc528839e4383b06129043c903f60e6
Moon Bombo <bombo96@naver.com>
2022-01-17 20:29:41 +0900

    Add starbucks youtube iframe

---
+++ js/youtube.js
@@ -0,0 +1,24 @@
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+
+
+function onYouTubeIframeAPIReady() {
+  let player = new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 여상 ID
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      onReady: function(event) {
+        event.target.mute() // 음소거
+      }
+    }
+  });
+}
